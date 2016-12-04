9af21a00e56d10eb90eb0e63bbaf57e05e96b4dd
zeynepArslan <zeynep.arslan@ozu.edu.tr>
2016-12-04 20:44:08 +0200

    deneme

--- src/Apple.java
+++ src/Apple.java
@@ -3,6 +3,7 @@ public class Apple extends BodyShape {
 
 	public Apple(float gogus, float bel, float kalca) {
 		super(gogus, bel, kalca);
+		System.out.println("dfgh");
 		// TODO Auto-generated constructor stub
 	}
 	
